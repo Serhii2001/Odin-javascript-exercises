2fdd3923f8091ba16e4fcb3d2c0df0adba1384af
Serhii <sergejandreev2001@gmail.com>
2023-12-02 15:30:11 +0200

    commit first

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,18 @@
-const removeFromArray = function (array, rem) {
-	let x = 0;
-	for (i = 0; i < array.length; i++) {
-		if (array[i] === rem) {
-			x = array.splice(i, 1);
-		}
-	}
-	return array;
-};
-
 // const removeFromArray = function (array, rem) {
-// 	let index = array.indexOf(rem);
-// 	array.splice(index, 1);
+// 	let x = 0;
+// 	for (i = 0; i < array.length; i++) {
+// 		if (array[i] === rem) {
+// 			x = array.splice(i, 1);
+// 		}
+// 	}
 // 	return array;
-// }
+// };
+
+const removeFromArray = function (array, rem) {
+	let index = array.indexOf(rem);
+	array.splice(index, 1);
+	return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
