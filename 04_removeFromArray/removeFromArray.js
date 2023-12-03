96735bc95637a0116fb351c084699b992b6e16f4
serhii_code_wizard <sergejandreev2001@gmail.com>
2023-12-04 00:22:03 +0200

    Pass seven tests
    I do all test what is needed for lesson about arrays and loops. It was fun

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,27 @@
-// const removeFromArray = function (array, rem) {
-// 	let x = 0;
-// 	for (i = 0; i < array.length; i++) {
-// 		if (array[i] === rem) {
-// 			x = array.splice(i, 1);
+const removeFromArray = function (array, ...rems) {
+	const name = function (arg) {
+		let x = 0;
+		for (i = 0; i < array.length; i++) {
+			if (array[i] === arg) {
+				x = array.splice(i, 1);
+			}
+		}
+	}
+
+	rems.forEach(name);
+	return array;
+};
+
+// const removeFromArray = function (array, ...rems) {
+// 	const remove = (rem) => {
+// 		let index = array.indexOf(rem);
+// 		if (index > -1) {
+// 			array.splice(index, 1);
 // 		}
 // 	}
+// 	rems.forEach(remove);
 // 	return array;
-// };
-
-const removeFromArray = function (array, rem) {
-	let index = array.indexOf(rem);
-	array.splice(index, 1);
-	return array;
-}
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function (sum1, sum2) {
+	if (sum1 < 0 || sum2 < 0 || typeof sum2 != 'number') {
+		return 'ERROR';
+	}
+
+	let sum = 0;
+	if (sum1 < sum2) {
+		for (let i = sum1; i <= sum2; i++) {
+			sum += i;
+
+		}
+	} else {
+		for (let i = sum2; i <= sum1; i++) {
+			sum += i;
+
+		}
+	}
+	return sum;
 
 };
 
